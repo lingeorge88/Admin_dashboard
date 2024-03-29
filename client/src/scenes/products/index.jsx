883a4cf84a033c1d29a0130dd7d99810776c3f02
lingeorge88 <lingeorge04@gmail.com>
2024-03-28 19:33:12 -0700

    finished customers api and page

--- client/src/scenes/products/index.jsx
+++ client/src/scenes/products/index.jsx
@@ -75,7 +75,6 @@ const Product = ({ _id, name, description, price, rating, category, supply, stat
 const Products = () => {
     const {data, isLoading} = useGetProductsQuery();
     const isNonMobile = useMediaQuery("(min-width: 1000px)");
-    console.log("data", data);
   return (
     <Box m="1.5rem 2.5rem">
       <Header title="Products" subtitle="See your list of products." />
