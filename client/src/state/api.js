0107ad368b6b9733a9de9811e6cc876405f081a7
lingeorge88 <lingeorge04@gmail.com>
2024-04-03 17:23:45 -0700

    added adminpage table with mui v6 menu item customization

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -44,7 +44,7 @@ export const api = createApi({
           providesTags: ["Sales"],
         }),
         getAdmins: build.query({
-          query: () => `management/sales`,
+          query: () => `management/admins`,
           providesTags: ["Admins"],
         }),
     }),
